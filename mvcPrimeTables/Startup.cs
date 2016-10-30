6e64cbc4dd9605a860dd95cc04656d17a04c032f
Mahdiya Akseen <mahdiyaakseen@gmail.com>
2016-10-30 10:38:27 +0000

    Add project files.

---
+++ mvcPrimeTables/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcPrimeTables.Startup))]
+namespace mvcPrimeTables
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
